af2d5c4f6997b6bbef357f9a2bed90a29b7e8226
webersec <83569731+webersec@users.noreply.github.com>
2024-07-18 15:59:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Arrow from './icons/Arrow';
 import { bear, coin, highVoltage, notcoin, rocket, trophy } from './images';
 
 const App = () => {
-  const [points, setPoints] = useState<number>(29857775);
-  const [energy, setEnergy] = useState<number>(2532);
+  const [points, setPoints] = useState(29857775);
+  const [energy, setEnergy] = useState(2532);
   const [clicks, setClicks] = useState<{ id: number, x: number, y: number }[]>([]);
-  const pointsToAdd: number = 12;
-  const energyToReduce: number = 12;
+  const pointsToAdd = 12;
+  const energyToReduce = 12;
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (energy - energyToReduce < 0) {
@@ -19,7 +19,7 @@ const App = () => {
     const y = e.clientY - rect.top;
 
     setPoints(points + pointsToAdd);
-    setEnergy((prevEnergy) => Math.max(0, prevEnergy - energyToReduce));
+    setEnergy(energy - energyToReduce < 0 ? 0 : energy - energyToReduce);
     setClicks([...clicks, { id: Date.now(), x, y }]);
   };
 
@@ -30,92 +30,82 @@ const App = () => {
   // useEffect hook to restore energy over time
   useEffect(() => {
     const interval = setInterval(() => {
-      setEnergy((prevEnergy) => Math.min(prevEnergy + 10, 6500));
-    }, 1000); // Restore 10 energy points every second
+      setEnergy((prevEnergy) => Math.min(prevEnergy + 1, 6500));
+    }, 100); // Restore 10 energy points every second
 
-    return () => clearInterval(interval); // Clean up interval on component unmount
+    return () => clearInterval(interval); // Clear interval on component unmount
   }, []);
 
   return (
     <div className="bg-gradient-main min-h-screen px-4 flex flex-col items-center text-white font-medium">
-      {/* Background overlays */}
+
       <div className="absolute inset-0 h-1/2 bg-gradient-overlay z-0"></div>
       <div className="absolute inset-0 flex items-center justify-center z-0">
         <div className="radial-gradient-overlay"></div>
       </div>
 
       <div className="w-full z-10 min-h-screen flex flex-col items-center text-white">
-        {/* Top section with Join squad link */}
+
         <div className="fixed top-0 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white">
           <div className="w-full cursor-pointer">
-            <div className="bg-[#1f1f1f] text-center py-2 rounded-xl"">
-              <p className="text-lg">
-                <a href="https://t.me/arkcoinofficial">
-                  Join squad <Arrow size={18} className="ml-0 mb-1 inline-block" />
-                </a>
-              </p>
-            </div>
+  <a href="https://t.me/arkcoinofficial/" className="bg-[#1f1f1f] text-center py-2 rounded-xl">
+    <p className="text-lg">Join squad <Arrow size={18} className="ml-0 mb-1 inline-block" /></p>
+  </a>
+</div>
+          <div className="mt-12 text-5xl font-bold flex items-center">
+            <img src={coin} width={44} height={44} />
+            <span className="ml-2">{points.toLocaleString()}</span>
+          </div>
+          <div className="text-base mt-2 flex items-center">
+            <img src={trophy} width={24} height={24} />
+            <span className="ml-1">Gold <Arrow size={18} className="ml-0 mb-1 inline-block" /></span>
           </div>
         </div>
 
-        {/* Points and Gold sections */}
-        <div className="mt-12 text-5xl font-bold flex items-center">
-          <img src={coin} width={44} height={44} alt="Coin" />
-          <span className="ml-2">{points.toLocaleString()}</span>
-        </div>
-        <div className="text-base mt-2 flex items-center">
-          <img src={trophy} width={24} height={24} alt="Trophy" />
-          <span className="ml-1">Gold <Arrow size={18} className="ml-1 inline-block" /></span>
-        </div>
 
-        {/* Bottom section with energy bar and buttons */}
         <div className="fixed bottom-0 left-0 w-full px-4 pb-4 z-10">
           <div className="w-full flex justify-between gap-2">
-            {/* Energy display */}
             <div className="w-1/3 flex items-center justify-start max-w-32">
               <div className="flex items-center justify-center">
                 <img src={highVoltage} width={44} height={44} alt="High Voltage" />
                 <div className="ml-2 text-left">
                   <span className="text-white text-2xl font-bold block">{energy}</span>
-                  <span className="text-white opacity-75">/ 6500</span>
+                  <span className="text-white text-large opacity-75">/ 6500</span>
                 </div>
               </div>
             </div>
-            {/* Buttons section */}
             <div className="flex-grow flex items-center max-w-60 text-sm">
-              <div className="w-full bg-yellow-400 py-4 rounded-2xl flex justify-around">
+              <div className="w-full bg-[#fad258] py-4 rounded-2xl flex justify-around">
                 <button className="flex flex-col items-center gap-1">
-                  <img src={bear} width={24} height={24} alt="Bear" />
+                  <img src={bear} width={24} height={24} alt="High Voltage" />
                   <span>Frens</span>
                 </button>
-                <div className="h-[48px] w-[2px] bg-yellow-500"></div>
+                <div className="h-[48px] w-[2px] bg-[#fddb6d]"></div>
                 <button className="flex flex-col items-center gap-1">
-                  <img src={coin} width={24} height={24} alt="Coin" />
+                  <img src={coin} width={24} height={24} alt="High Voltage" />
                   <span>Earn</span>
                 </button>
-                <div className="h-[48px] w-[2px] bg-yellow-500"></div>
+                <div className="h-[48px] w-[2px] bg-[#fddb6d]"></div>
                 <button className="flex flex-col items-center gap-1">
-                  <img src={rocket} width={24} height={24} alt="Rocket" />
+                  <img src={rocket} width={24} height={24} alt="High Voltage" />
                   <span>Boosts</span>
                 </button>
               </div>
             </div>
           </div>
-          {/* Energy bar */}
-          <div className="w-full bg-yellow-500 rounded-full mt-4">
-            <div className="bg-gradient-to-r from-yellow-400 to-yellow-200 h-4 rounded-full" style={{ width: `${(energy / 6500) * 100}%` }}></div>
+          <div className="w-full bg-[#f9c035] rounded-full mt-4">
+            <div className="bg-gradient-to-r from-[#f3c45a] to-[#fffad0] h-4 rounded-full" style={{ width: `${(energy / 6500) * 100}%` }}></div>
           </div>
         </div>
 
-        {/* Main clickable area */}
+
         <div className="flex-grow flex items-center justify-center">
           <div className="relative mt-4" onClick={handleClick}>
-            <img src={notcoin} width={256} height={256} alt="Notcoin" />
-            {/* Display clicks */}
+            <img src={notcoin} width={256} height={256} alt="notcoin" />
             {clicks.map((click) => (
               <div
                 key={click.id}
-                className="absolute text-5xl font-bold"
+                className="absolute text-5xl font-bold opacity-0"
                 style={{
                   top: `${click.y - 42}px`,
                   left: `${click.x - 28}px`,
@@ -123,7 +113,7 @@ const App = () => {
                 }}
                 onAnimationEnd={() => handleAnimationEnd(click.id)}
               >
-                +{pointsToAdd}
+                12
               </div>
             ))}
           </div>
