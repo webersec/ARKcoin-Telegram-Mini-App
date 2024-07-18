9b70e0566c94eeb475c33149c2150bd0a282ca0f
webersec <83569731+webersec@users.noreply.github.com>
2024-07-18 15:18:05 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const App = () => {
         <div className="fixed top-0 left-0 w-full px-4 pt-8 z-10 flex flex-col items-center text-white">
           <div className="w-full cursor-pointer">
             <div className="bg-[#1f1f1f] text-center py-2 rounded-xl">
-              <a href="https://t.me/arkcoinofficial"><p className="text-lg">Join squad <Arrow size={18} className="ml-0 mb-1 inline-block" /></p></div>a>
+              <a href={"https://t.me/arkcoinofficial"}><p className="text-lg">Join squad <Arrow size={18} className="ml-0 mb-1 inline-block" /></p></div>a>
             </div>
           </div>
           <div className="mt-12 text-5xl font-bold flex items-center">
